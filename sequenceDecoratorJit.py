a210026539adb242af74f7313b7f3810a97695c4
nomiku1999 <tranhuyphuong99@gmail.com>
2022-05-18 21:00:31 +0700

    Update sequenceDecoratorJit.py

--- sequenceDecoratorJit.py
+++ sequenceDecoratorJit.py
@@ -1,6 +1,8 @@
 import numba
 import numpy as np
 
+INF = 123456789
+
 f = open("graph.txt", "r")
 vertices = int(f.readline())
 g_pyObj = []
@@ -18,65 +20,80 @@ for i in range(vertices):
 
 bet = np.zeros((vertices, vertices), dtype=float)
 
-#@numba.jit(nopython=True)
-def bfs(x : int, g : np.array, bet : np.array):
+
+def bfs(start, g, bet):
     visited = np.zeros(vertices, dtype=int)
     level = np.empty(vertices, dtype=int)
+    level.fill(INF)
     parents = np.zeros(vertices, dtype=float)
     point = np.empty(vertices, dtype=float)
     point.fill(1)
-    level.fill(12345)
-    q = np.zeros(vertices, dtype=int) # put/ get/ empty
-    l, r = 0, 0 # left right is begin and end of queue
-    q[r] = x; r += 1 # put operator
-    level[x], parents[x] = 0, 1; maxlv = 0; visited[x] = 1
+    q = np.zeros(vertices, dtype=int)
+
+    bfs1(q, g, level, parents, visited, start)
+
+    lv = np.empty((15, vertices), dtype=int)
+    sizeOfLv = np.zeros(15, dtype=int)
+    bfs2(g, lv, sizeOfLv, level, parents, point, bet)
+
+
+@numba.jit(nopython=True)
+def bfs1(q, g, level, parents, visited, start):
+    l, r = 0, 0  # left right is begin and end of queue
+    q[r] = start
+    r += 1  # put operator
+
+    level[start], parents[start] = 0, 1
+    visited[start] = 1
     while l < r:
         u = q[l]
         l += 1       # pop operator
         for vindex in range(sizeOfG[u]):
             v = g[u][vindex]
-            if level[v] + 1 == level[u]: # meet your parent vertices
-                parents[u] += 1
+            if level[v] + 1 == level[u]:  # meet your parent vertices
+                parents[u] += parents[v]
                 continue
-            if not visited[v]: # meet new vertices
+            if not visited[v]:  # meet new vertices
                 visited[v] = 1
                 level[v] = level[u] + 1
-                if maxlv < level[v]:
-                    maxlv = level[v]
-                q[r] = v; r += 1 # put operator
+                q[r] = v
+                r += 1  # put operator
 
-    # lv = [[] for i in range(maxlv + 1)]
-    lv = np.empty((maxlv + 1, vertices), dtype=int)
-    sizeOfLv = np.zeros(maxlv + 1, dtype=int)
+
+@numba.jit(nopython=True)
+def bfs2(g, lv, sizeOfLv, level, parents, point, bet):
+
+    maxlv = 0
     for i in range(vertices):
-        if level[i] != 12345:
+        if level[i] != INF:
             newPosition = sizeOfLv[level[i]]
             lv[level[i]][newPosition] = i
             sizeOfLv[level[i]] += 1
-
-    # print(lv)
-    # print(sizeOfLv)
-    # print(parents)
+            if maxlv < level[i]:
+                maxlv = level[i]
 
     for leafLevel in range(maxlv, 0, -1):
-        for leafIndex in range(sizeOfLv[leafLevel]): #lv[lvleaf]:
+        for leafIndex in range(sizeOfLv[leafLevel]):  # lv[lvleaf]:
             # leaf vertices if lv[leaf]
             leaf = lv[leafLevel][leafIndex]
 
             for connectedNodeIndex in range(sizeOfG[leaf]):
-                node = g[leaf][connectedNodeIndex]                
+                node = g[leaf][connectedNodeIndex]
                 if level[node] + 1 == leafLevel:
-                    if parents[leaf] == 0: continue
+                    if parents[leaf] == 0:
+                        continue
                     # print("parent, leaf: ", node, leaf)
                     # print("point parent", point[leaf], parents[leaf])
-                    bet[node][leaf] += point[leaf] / parents[leaf]
-                    bet[leaf][node] += point[leaf] / parents[leaf]
-                    point[node] += point[leaf] / parents[leaf]
+                    gainPoint = point[leaf] / parents[leaf] * parents[node]
+                    bet[node][leaf] += gainPoint
+                    bet[leaf][node] += gainPoint
+                    point[node] += gainPoint
                     # print("Bet", bet[leaf][node], bet[node][leaf])
                     # print("point", point[node])
 
-normalizationFactor = vertices ** 2 - vertices + 1 # C=(n-1)^{2}-(n-1)
+
 resBet = []
+
 
 def betweenness():
     # bfs(0, g, bet)
@@ -84,13 +101,13 @@ def betweenness():
         bfs(i, g, bet)
     for i in range(vertices):
         for j in range(vertices):
-            if j < i: continue
-            if bet[i][j]: 
-                bet[i][j] *= (2 / (vertices * (vertices - 1)))
+            if j < i:
+                continue
+            if bet[i][j]:
                 bet[i][j] /= 2
-                # print(f"({i}, {j}) {bet[i][j]:0.2f}")
                 resBet.append(f"({i}, {j}) {bet[i][j]:0.2f}")
+
 
 betweenness()
 # bet
-resBet
+print(resBet)
