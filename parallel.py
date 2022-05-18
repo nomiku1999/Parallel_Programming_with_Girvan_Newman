7b6b2c274420ecba8d1987d151d7791d079c3768
nomiku1999 <tranhuyphuong99@gmail.com>
2022-05-18 20:26:13 +0700

    Update parallel.py

--- parallel.py
+++ parallel.py
@@ -43,7 +43,7 @@ def bfs_kernel1(parent, visited, que, newque, edgeArray, numVertices, neighborsP
                 if que[visitVertice] == False:
                     # visit parent
                     if visited[visitVertice] == True:
-                        parent[tIdx] += 1
+                        parent[tIdx] += parent[visitVertice]
                     # visit child
                     else:
                         newque[visitVertice] = True
@@ -65,21 +65,109 @@ def bfs_kernel2(level, parent, visited, que, newque, edgeArray, numVertices, nei
             newque[tIdx] = False
 
 
-level = np.zeros(numVertices, dtype=int)
-parent = np.zeros(numVertices, dtype=int)
-visited = np.zeros(numVertices, dtype=bool)
-que = np.zeros(numVertices, dtype=bool)
-newque = np.zeros(numVertices, dtype=bool)
-GRID_SIZE = int(math.ceil(numVertices / BLOCK_SIZE))
-que[0] = True
+@cuda.jit
+def bfs_kernel3(bet, point, parent, level, visited, que, edgeArray, numVertices, neighborsPerVertex, currentLevel):
+    tIdx = cuda.grid(1)
 
-currentLevel = 1
-while currentLevel < 10:
-    bfs_kernel1[GRID_SIZE, BLOCK_SIZE](
-        parent, visited, que, newque, edgeArray, numVertices, neighborsPerVertex)
-    cuda.synchronize()
-    bfs_kernel2[GRID_SIZE, BLOCK_SIZE](
-        level, parent, visited, que, newque, edgeArray, numVertices, neighborsPerVertex, currentLevel)
-    cuda.synchronize()
-    currentLevel += 1
-    print(np.count_nonzero(visited))
+    if tIdx < numVertices:
+
+        if que[tIdx] == True:
+            startEdges, endEdges = tIdx * \
+                neighborsPerVertex, (tIdx + 1) * neighborsPerVertex
+
+            for edge in range(startEdges, endEdges, 1):
+                visitVertice = edgeArray[edge]
+                # not an edge
+                if visitVertice == INF:
+                    break
+                # only visit vertex diffent level
+                if que[visitVertice] == False:
+                    # visit parent
+                    if visited[visitVertice] == False and level[visitVertice] == currentLevel - 1:
+                        updatePoint = (point[tIdx] / parent[tIdx]) * parent[visitVertice]
+                        cuda.atomic.add(bet, edge, updatePoint)
+                        cuda.atomic.add(point, visitVertice, updatePoint)
+
+
+@cuda.jit
+def bfs_kernel4(que, visited, edgeArray, numVertices):
+    tIdx = cuda.grid(1)
+
+    if tIdx < numVertices:
+        if que[tIdx] == True:
+            que[tIdx] = False
+            visited[tIdx] = True
+
+
+@cuda.jit
+def bfs_kernel5(que, level, edgeArray, numVertices, currentLevel):
+    tIdx = cuda.grid(1)
+
+    if tIdx < numVertices:
+        if level[tIdx] == currentLevel:
+            que[tIdx] = True
+
+bet = np.zeros(numEdges, dtype=float)
+
+
+def bfs(start):
+    level = np.empty(numVertices, dtype=int); level.fill(INF)
+    parent = np.zeros(numVertices, dtype=float)
+    visited = np.zeros(numVertices, dtype=bool)
+    que = np.zeros(numVertices, dtype=bool)
+    newque = np.zeros(numVertices, dtype=bool)
+    point = np.zeros(numVertices, dtype=float)
+    point.fill(1)
+    GRID_SIZE = int(math.ceil(numVertices / BLOCK_SIZE))
+    que[start] = True
+    parent[start] = 1
+    level[start] = 0
+
+    currentLevel1 = 0
+    while currentLevel1 < 15:
+        bfs_kernel1[GRID_SIZE, BLOCK_SIZE](
+            parent, visited, que, newque, edgeArray, numVertices, neighborsPerVertex)
+        cuda.synchronize()
+        bfs_kernel2[GRID_SIZE, BLOCK_SIZE](
+            level, parent, visited, que, newque, edgeArray, numVertices, neighborsPerVertex, currentLevel1)
+        currentLevel1 += 1
+
+    que = np.zeros(numVertices, dtype=bool)
+    visited = np.zeros(numVertices, dtype=bool)
+    point = np.empty(numVertices, dtype=float)
+    point.fill(1)
+
+    maxLevel = 0
+    for i in range(numVertices):
+        if level[i] != INF:
+            maxLevel = max(maxLevel, level[i])
+
+    for i in range(numVertices):
+        if level[i] == maxLevel:
+            que[i] = True
+
+    currentLevel2 = maxLevel
+
+    while currentLevel2 >= 0:
+        bfs_kernel3[GRID_SIZE, BLOCK_SIZE](
+            bet, point, parent, level, visited, que, edgeArray, numVertices, neighborsPerVertex, currentLevel2)
+        cuda.synchronize()
+        currentLevel2 -= 1
+        bfs_kernel4[GRID_SIZE, BLOCK_SIZE](
+                que, visited, edgeArray, numVertices)
+        cuda.synchronize()
+        bfs_kernel5[GRID_SIZE, BLOCK_SIZE](
+                que, level, edgeArray, numVertices, currentLevel2)
+        cuda.synchronize()
+
+for i in range(numVertices):
+    bfs(i)
+
+
+for i in range(numVertices):
+    for j in range(neighborsPerVertex):
+        id = i * neighborsPerVertex + j
+        if edgeArray[id] != INF:
+            if(bet[id] != 0):
+                print(f"({i}, {edgeArray[id]}) {bet[id]:0.2f}")
+# (0, 1) 638.07', '(0, 3) 1400.88', '(0, 5) 170.34', '(0, 9) 387.88', '(0, 11) 189.00'
